6841603d0b23f5fb7690ad93b82354b5f83a056a
UFOMelkor <oskar@programming-php.net>
2013-10-31 01:52:50 +0100

    autoloading-fixes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,12 @@
 <?php
-chdir(__DIR__);
-
 $loader = null;
-if (file_exists('../vendor/autoload.php')) {
-    $loader = include '../vendor/autoload.php';
-} elseif (file_exists('../../../autoload.php')) {
-    $loader = include '../../../autoload.php';
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    $loader = include __DIR__ . '/../vendor/autoload.php';
+} elseif (file_exists(__DIR__ '/../../../autoload.php')) {
+    $loader = include __DIR__ . '/../../../autoload.php';
 } else {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }
 
 /* @var $loader \Composer\Autoload\ClassLoader */
-$loader->add('DkplusActionArgumentsTest', __DIR__);
+$loader->add('DkplusActionArgumentsTest\\', __DIR__);
