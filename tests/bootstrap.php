3d54b365e323f006919783b849fa1d0789f1607e
UFOMelkor <oskar@programming-php.net>
2013-10-31 01:57:46 +0100

    autoloading-fixes for testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 $loader = null;
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     $loader = include __DIR__ . '/../vendor/autoload.php';
-} elseif (file_exists(__DIR__ '/../../../autoload.php')) {
+} elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
     $loader = include __DIR__ . '/../../../autoload.php';
 } else {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
